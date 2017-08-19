82e34d5e82ce29833180b66df30b2c26c687a80b
MaverickMeme <yelllukesky@gmail.com>
2017-08-19 23:37:27 +0100

    Tiny Update
    Read steam change notes for more info

--- lua/entities/mav_moneypickup/shared.lua
+++
@@ -1,6 +0,0 @@
-ENT.Type = "anim"
-
-ENT.PrintName = "Money Pickup"
-ENT.Author = "MaverickMeme"
-ENT.Category = "MaverickEntities"
-ENT.Spawnable = true
---
+++ lua/entities/simple_event_pickup/shared.lua
@@ -0,0 +1,6 @@
+ENT.Type = "anim"
+
+ENT.PrintName = "SimpleEvent - Pickup"
+ENT.Author = "CL34R"
+ENT.Category = "CL34R_ENTS"
+ENT.Spawnable = true
